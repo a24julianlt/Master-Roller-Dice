a4a144cbb6b7590bed70c31e02100b6764c4dc3a
Julián López Triñanes <a24julianlt@iessanclemente.net>
2025-12-07 00:53:54 +0100

    añadido fuente, y empezando con InicioFragment.kt

--- app/src/main/java/com/example/masterrollerdice/InicioFragment.kt
+++ app/src/main/java/com/example/masterrollerdice/InicioFragment.kt
@@ -1,5 +1,7 @@
 package com.example.masterrollerdice
 
+import android.animation.ValueAnimator
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -26,7 +28,35 @@ class InicioFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
+        // Bloque modificador
+        var expandido = false
+
+        val bloque = view.findViewById<View>(R.id.bloqueExpandable)
+
+        bloque.setOnClickListener {
+            expandido = !expandido
+
+            val alturaInicial = if (expandido) 80 else 180   // dp pequeño ↔ grande
+            val alturaFinal = if (expandido) 180 else 80   // dp grande ↔ pequeño
+
+            val from = dpToPx(requireContext(), alturaInicial)
+            val to = dpToPx(requireContext(), alturaFinal)
+
+            val anim = ValueAnimator.ofInt(from, to)
+            anim.duration = 300
+            anim.addUpdateListener {
+                val params = bloque.layoutParams
+                params.height = it.animatedValue as Int
+                bloque.layoutParams = params
+            }
+            anim.start()
+        }
+
     }
+
+    fun dpToPx(context: Context, dp: Int): Int =
+        (dp * context.resources.displayMetrics.density).toInt()
+
 
     override fun onDestroyView() {
         super.onDestroyView()
