4d20a753f0132250a1fc97e73635121c223fb137
Julián López Triñanes <a24julianlt@iessanclemente.net>
2025-12-07 19:58:18 +0100

    Añadido fondo a drawer, falta poner texto en negro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
 dependencies {
     implementation("com.google.code.gson:gson:2.10.1")
     implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.material.v1100)
+
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
--- app/src/main/java/Dado.kt
+++ app/src/main/java/Dado.kt
@@ -1,1 +1,16 @@
-data class Dado(val caras: Int, var resultado: Int? = null)
+data class Dado(val caras: Int, var resultado: Int? = null) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Dado
+
+        if (caras != other.caras) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return caras
+    }
+}
